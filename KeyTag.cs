2ca718dbd5040ee942e243762b5f619536375b12
gwnrobin <robinschellingerhout@live.nl>
2017-11-22 15:39:34 +0100

    scripts

---
+++ KeyTag.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyTag : MonoBehaviour
+{ 
+    [SerializeField]
+    private GameObject door;
+    public bool doDestroy = false;
+
+    void Update()
+    {
+        if(doDestroy)
+        {
+            Destroy(door);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Playe : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject text;
+    private int CollectablesCollected;
+    private int keyTag;
+    private Vector3 startPosition;
+    private int deathMessageCount;
+
+    public TimeManager theTimeManager;
+    public ScoreManager theScoreManager;
+    private Animator chestAnim;
+
+    public int theSecondsToWait;
+
+    public GameObject theDeathMenu;
+
+    void Start()
+    {
+
+        chestAnim = GameObject.Find("Chest").GetComponent<Animator>();
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+//        print(startPosition);
+        if (text.GetComponent<Text>().enabled == true)
+        {
+            deathMessageCount++;
+        }
+        if(deathMessageCount == 90)
+        {
+            text.GetComponent<Text>().enabled = false;
+            deathMessageCount = 0;
+        }
+
+        //Keybinding test for stopping timer.
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            /*theTimeManager.StopCounting();*/
+        }
+
+        //Keybinding test for coins collecting.
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            theScoreManager.AddValueToScore(1);
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Collision with collectable.
+        if (other.tag == "collectable+")
+        {
+            CollectablesCollected++;
+            Destroy(other.gameObject);
+            theScoreManager.AddValueToScore(1);
+        }
+
+        //Collsion with key.
+        if (other.tag == "key")
+        {
+            other.gameObject.GetComponent<KeyTag>().doDestroy = true;
+        }
+
+        //Collision with spikes.
+        if (other.tag == "death")
+        {
+            transform.position = startPosition;
+            theDeathMenu.SetActive(true);
+            Time.timeScale = 0f;
+            /*text.GetComponent< Text>().enabled = true;*/
+        }
+
+        //Collision with finish.
+        if (other.tag == "Finish")
+        {
+            chestAnim.Play("Open", 0, 0);
+            StartCoroutine(OnFinish());
+        }
+
+    }
+
+    IEnumerator OnFinish()
+    {
+        /*theTimeManager.StopCounting();*/
+        yield return new WaitForSeconds(theSecondsToWait);
+        SceneManager.LoadScene(Application.loadedLevel +1);
+        /*theTimeManager.ResetCounting();*/
+    }
+
+}
